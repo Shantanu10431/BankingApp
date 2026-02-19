e1ea0c7e130525e4efd21c0dbf55017442439dc5
Shantanu <shantanuk10431@gmail.com>
2026-02-19 18:45:52 +0530

    Added_explicit_DB_connection_for_Vercel

--- api/index.js
+++ api/index.js
@@ -1,3 +1,7 @@
 const app = require('../server/server');
+const { connectDB } = require('../server/config/database');
+
+// Ensure database connects in serverless environment
+connectDB();
 
 module.exports = app;
